48957aaf9663a5d8809e112c77bb1891087cc0dd
samaikatayyba64-codes <samaikatayyba64@gmail.com>
2025-10-15 20:43:12 +0500

    week_04

---
+++ week_04_exercise_01_02_03.cs
@@ -0,0 +1,47 @@
+﻿//Objective: Create and access a tuple’s elements.
+
+using System;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main()
+    {
+        (string, int, int) score = ("R2-D2", 12420, 15);
+
+        Console.WriteLine($"Name: {score.Item1}");
+        Console.WriteLine($"Points: {score.Item2}");
+        Console.WriteLine($"Level: {score.Item3}");
+    }
+}
+
+
+//Objective: Use named tuple elements for readability.
+
+class Program
+{
+    static void Main()
+    {
+        var player = (Name: "C-3PO", Points: 8543, Level: 9);
+        Console.WriteLine($"{player.Name} reached Level {player.Level} with {player.Points} points.");
+    }
+}
+
+
+
+//Objective: Return and deconstruct tuples from a method.
+
+
+class Program
+{
+    static (string, int, int) GetScore()
+    {
+        return ("GONK", -1, 1);
+    }
+
+    static void Main()
+    {
+        (string name, int points, int level) = GetScore();
+        Console.WriteLine($"{name} reached Level {level} with {points} points.");
+    }
+}
---
+++ week_04_exercise_04_05_06_07.cs
@@ -0,0 +1,74 @@
+﻿//Objective: Extract only required tuple elements.
+
+using System.ComponentModel;
+
+class Program
+{
+    static void Main()
+    {
+        var score = (Name: "R2-D2", Points: 12420, Level: 15);
+        (string name, _, _) = score;
+        Console.WriteLine($"Player: {name}");
+    }
+}
+
+
+//Objective: Combine enumerations inside a tuple.
+
+enum Recipe { Soup, Stew, Gumbo }
+enum Ingredient { Mushrooms, Chicken, Carrots, Potatoes }
+enum Seasoning { Spicy, Salty, Sweet }
+
+class Program
+{
+    static void Main()
+    {
+        var soup = (Recipe: Recipe.Gumbo, Ingredient: Ingredient.Chicken, Seasoning: Seasoning.Sweet);
+        Console.WriteLine($"{soup.Seasoning} {soup.Ingredient} {soup.Recipe}");
+    }
+}
+
+
+
+//Objective: Define a class with fields and create an instance.
+
+class Score
+{
+    public string name;
+    public int points;
+    public int level;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Score s = new Score();
+        s.name = "R2-D2";
+        s.points = 12420;
+        s.level = 15;
+        Console.WriteLine($"{s.name} - Level {s.level} - Points: {s.points}");
+    }
+}
+
+
+
+//Objective: Add class methods that act on data.
+
+
+class Score
+{
+    public string name;
+    public int points;
+    public int level;
+    public bool EarnedStar() => (points / level) > 1000;
+}
+class Program
+{
+    static void Main()
+    {
+        Score s = new Score { name = "C-3PO", points = 8543, level = 9 };
+        if (s.EarnedStar())
+            Console.WriteLine($"{s.name} earned a star!");
+    }
+}
